ffb070991a79378298a686305e5f079bb7ca0186
vixfwis <existencialis@gmail.com>
2018-12-01 14:33:56 +0300

    Change maps models

--- maps/urls.py
+++ maps/urls.py
@@ -4,9 +4,6 @@ import maps.views
 
 router = routers.SimpleRouter()
 router.register(r'map', maps.views.MapViewSet)
-router.register(r'options', maps.views.OptionsViewSet)
-router.register(r'mapmods', maps.views.MapModsViewSet)
-router.register(r'contributors', maps.views.ContributorsViewSet)
 router.register(r'mod', maps.views.ModViewSet)
 
 urlpatterns = router.urls
--- maps/views.py
+++ maps/views.py
@@ -2,12 +2,9 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 
-from maps.models import Map, Options, MapMods, Contributors, Mod
+from maps.models import Map, Mod
 from maps.serializers import (
     MapSerializer,
-    OptionsSerializer,
-    MapModsSerializer,
-    ContributorsSerializer,
     ModSerializer,
 )
 
@@ -15,24 +12,6 @@ from maps.serializers import (
 class MapViewSet(viewsets.ModelViewSet):
     queryset = Map.objects.all()
     serializer_class = MapSerializer
-    permission_class = [AllowAny]
-
-
-class OptionsViewSet(viewsets.ModelViewSet):
-    queryset = Options.objects.all()
-    serializer_class = OptionsSerializer
-    permission_class = [AllowAny]
-
-
-class MapModsViewSet(viewsets.ModelViewSet):
-    queryset = MapMods.objects.all()
-    serializer_class = MapModsSerializer
-    permission_class = [AllowAny]
-
-
-class ContributorsViewSet(viewsets.ModelViewSet):
-    queryset = Contributors.objects.all()
-    serializer_class = ContributorsSerializer
     permission_class = [AllowAny]
 
 
